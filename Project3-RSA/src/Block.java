f510176e4e7aa072c811df2df6ac8b8a0f64e96c
Andrew Podgorski <apodgo2@uic.edu>
2016-03-15 15:50:05 -0500

    Fixed indexing bug in Block

--- Project3-RSA/src/Block.java
+++ Project3-RSA/src/Block.java
@@ -8,33 +8,35 @@
  */
 
 public class Block {
-	int length = 0;
-	HUI storage = null;
-	
-	public Block(HUI in) {
-		this.storage = in;
-	}
-	
-	public HUI getHUI() {
-		return storage;
-	}
-	
-	public char[] getChars() {
-		char[] builtChars = new char[(storage.getArray().length/2)+1];
-		int currentIndex = 0;
-		int lastInt = -1;
-		//get pairs of integers from array
-		for (int x : storage.getArray()) {
-			//if this is the second integer in a pair
-			if (currentIndex % 2 != 0 && currentIndex != 0) {
-				//add the character representation of the pair to our storage
-				builtChars[currentIndex] = (char) ((lastInt*10)+x);
-			} else {
-				//otherwise just store the first int of a pair
-				lastInt = x;
-			}
-			currentIndex++;
-		}
-		return builtChars;
-	}
+ int length = 0;
+ HUI storage = null;
+ 
+ public Block(HUI in) {
+  this.storage = in;
+ }
+ 
+ public HUI getHUI() {
+  return storage;
+ }
+ 
+ public char[] getChars() {
+  char[] builtChars = new char[(storage.getArray().length/2)+1];
+  int currentIndex = 0;
+  int charIndex = 0;
+  int lastInt = -1;
+  //get pairs of integers from array
+  for (int x : storage.getArray()) {
+   //if this is the second integer in a pair
+   if (currentIndex % 2 != 0 && currentIndex != 0) {
+    //add the character representation of the pair to our storage
+    builtChars[charIndex] = (char) ((lastInt*10)+x);
+    charIndex++;
+   } else {
+    //otherwise just store the first int of a pair
+    lastInt = x;
+   }
+   currentIndex++;
+  }
+  return builtChars;
+ }
 }
