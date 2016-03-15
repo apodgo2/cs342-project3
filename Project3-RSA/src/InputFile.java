e699f5642213540d4c87a65670b99c7f7904f0af
Andrew Podgorski <apodgo2@uic.edu>
2016-03-15 16:40:29 -0500

    Updated KeyFile to parse XML

--- Project3-RSA/src/InputFile.java
+++ Project3-RSA/src/InputFile.java
@@ -11,92 +11,96 @@
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.File;
 
 public class InputFile {
-	protected String path = "";
-	protected String content = "";
-	protected boolean isInvalid = false;
-	protected boolean isWriteable = false;
+ protected String path = "";
+ protected String content = "";
+ protected boolean isInvalid = false;
+ protected boolean isWriteable = false;
+ protected File file = null;
 
-	public InputFile(String path) {
-		this.path = path;
-		this.isWriteable = true;
-		read();
-	}
-	public InputFile(String path, boolean isWriteable) {
-		this.path = path;
-		this.isWriteable = isWriteable;
-		read();
-	}
-	
-	public void read() {
-		FileReader in = null;
-	    
-		try {
-			in = new FileReader(path);
-		       
-		       int c;
-		       while ((c = in.read()) != -1) {
-		          content += c;
-		       }
-		    } catch (IOException e) {
-		    	System.out.println("You have specified an inappropriate filepath!");
-				e.printStackTrace();
-				path = null;
-				content = null;
-			}finally {
-		       if (in != null) {
-		          try {
-					in.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-		       }
-		    }
-	}
-	
-	public void write() {
-		if (isWriteable) {
-		FileWriter out = null;
-	    
-		try {
-			out = new FileWriter(path);
-		       
-		    out.write(content);
-		    } catch (IOException e) {
-		    	System.out.println("You have specified an inappropriate filepath!");
-				e.printStackTrace();
-				path = null;
-				content = null;
-			}finally {
-		       if (out != null) {
-		          try {
-					out.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-		       }
-		    }
-		}
-	}
-	
-	public String getPath() {
-		return path;
-	}
-	public String getContent() {
-		return content;
-	}
-	protected void invalidate() {
-		isInvalid = true;
-	}
-	public boolean isInvalid() {
-		return isInvalid;
-	}
-	
-	//override this:
-	public void parse() {
-		read();
-	}
+ public InputFile(String path) {
+  this.path = path;
+  this.isWriteable = true;
+  this.file = new File(path);
+  read();
+ }
+ public InputFile(String path, boolean isWriteable) {
+  this.path = path;
+  this.isWriteable = isWriteable;
+  this.file = new File(path);
+  read();
+ }
+ 
+ public void read() {
+  FileReader in = null;
+     
+  try {
+   in = new FileReader(path);
+         
+         int c;
+         while ((c = in.read()) != -1) {
+            content += c;
+         }
+      } catch (IOException e) {
+       System.out.println("You have specified an inappropriate filepath!");
+    e.printStackTrace();
+    path = null;
+    content = null;
+   }finally {
+         if (in != null) {
+            try {
+     in.close();
+    } catch (IOException e) {
+     // TODO Auto-generated catch block
+     e.printStackTrace();
+    }
+         }
+      }
+ }
+ 
+ public void write() {
+  if (isWriteable) {
+  FileWriter out = null;
+     
+  try {
+   out = new FileWriter(path);
+         
+      out.write(content);
+      } catch (IOException e) {
+       System.out.println("You have specified an inappropriate filepath!");
+    e.printStackTrace();
+    path = null;
+    content = null;
+   }finally {
+         if (out != null) {
+            try {
+     out.close();
+    } catch (IOException e) {
+     // TODO Auto-generated catch block
+     e.printStackTrace();
+    }
+         }
+      }
+  }
+ }
+ 
+ public String getPath() {
+  return path;
+ }
+ public String getContent() {
+  return content;
+ }
+ protected void invalidate() {
+  isInvalid = true;
+ }
+ public boolean isInvalid() {
+  return isInvalid;
+ }
+ 
+ //override this:
+ public void parse() {
+  read();
+ }
 }
--- Project3-RSA/src/KeyFile.java
+++ Project3-RSA/src/KeyFile.java
@@ -7,24 +7,59 @@
  * This is the class that will allow the RSA computation to deal with smaller numbers
  */
 
-//TODO: NOTE: Assuming that a KeyFile is composed of a 2 HUI's, one per line, in a text file. This means it can be read using the same methods as ResourceFile.
+//Apparently keyfiles are in XML. :/
 
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
 import java.io.IOException;
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+import java.io.*;
 
-public class KeyFile extends ResourceFile {
+public class KeyFile extends InputFile {
+  DocumentBuilderFactory factory;
+  DocumentBuilder builder;
+  HUI p1; HUI p2;
+  boolean isPublicKey = false;//determine type of primes we're working with
 
-	public KeyFile(String path) throws IOException {
-		super(path);
-		if (primes.size() > 2) {
-			throw new IOException("Incorrect file specified!");
-		}
-	}
-	
-	public HUI getPrime1() {
-		return primes.get(0);
-	}
-	public HUI getPrime2() {
-		return primes.get(1);
-	}
-	
+ public KeyFile(String path, boolean isPublicKey) {
+  super(path);
+  this.isPublicKey = isPublicKey;
+  //build our XML parsing equipment
+  try {
+    this.factory = DocumentBuilderFactory.newInstance();
+    this.builder = factory.newDocumentBuilder();
+  } catch (ParserConfigurationException e) {
+     e.printStackTrace();
+  }
+ }
+ 
+ @Override
+ public void parse() {
+   try {
+     if (isPublicKey) {
+       Document doc = builder.parse(this.file);
+       System.out.println("public rsakey evalue: "+doc.getElementById("rsakey").getAttribute("dvalue")+"rsakey nvalue: "+doc.getElementById("rsakey").getAttribute("nvalue"));
+     } else {
+       Document doc = builder.parse(this.file);
+       System.out.println("private rsakey dvalue: "+doc.getElementById("rsakey").getAttribute("dvalue")+"rsakey nvalue: "+doc.getElementById("rsakey").getAttribute("nvalue"));
+     }
+   } catch (SAXException e) {
+     e.printStackTrace();
+   } catch (IOException e) {
+     e.printStackTrace();
+   }
+ }
+ 
+ public HUI getPrime1() {
+  return p1;
+ }
+ public HUI getPrime2() {
+  return p2;
+ }
+ 
+ public boolean isPublicKey() {
+   return isPublicKey;
+ }
+ 
 }
